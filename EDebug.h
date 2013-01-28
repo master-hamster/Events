35fcfb4676eb9d0690e28ccfb91f376cb43fe6e2
Master Hamster <masterhamster@mail.ru>
2013-01-28 22:03:50 +0400

    Move debug definitions to EDebug.h

---
+++ EDebug.h
@@ -0,0 +1,21 @@
+#define DEBUG_EVENT 
+
+#ifdef DEBUG_EVENT
+//#define PRINTNAMES
+//#define DEBUG_EOBJECT
+//#define DEBUG_EVENTNAMES
+//#define DEBUG_EVENTSTACK
+//#define DEBUG_EINPUTDEVICE
+//#define DEBUG_EAPPLICATION
+//#define DEBUG_EBEEPER
+//#define DEBUG_ETIMER
+//#define DEBUG_EANALOGINPUT
+//#define DEBUG_EBUTTON
+//#define DEBUG_ELED
+//#define DEBUG_ERGBLED
+//#define DEBUG_ECANDLE
+//#define DEBUG_ECANDLE1
+//#define DEBUG_ETHERMO
+//
+#define DEBUG_ERROR 
+#endif
--- Events.h
+++ Events.h
@@ -27,28 +27,9 @@
 #include "Arduino.h"
 #endif
 
-//Uncomment next line for turn on debugging
-#define DEBUG_EVENT 
+#include "EDebug.h"
 
-#ifdef DEBUG_EVENT
-//#define PRINTNAMES
-//#define DEBUG_EOBJECT
-//#define DEBUG_EVENTNAMES
-//#define DEBUG_EVENTSTACK
-//#define DEBUG_EINPUTDEVICE
-//#define DEBUG_EAPPLICATION
-//#define DEBUG_EBEEPER
-//#define DEBUG_ETIMER
-//#define DEBUG_EANALOGINPUT
-//#define DEBUG_EBUTTON
-//#define DEBUG_ELED
-//#define DEBUG_ERGBLED
-//#define DEBUG_ECANDLE
-//#define DEBUG_ECANDLE1
-//#define DEBUG_ETHERMO
-//
-#define DEBUG_ERROR 
-#endif
+//Uncomment next line for turn on debugging
 
 
 //EVent NoEvent
